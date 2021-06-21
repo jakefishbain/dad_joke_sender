31737b52d742a1c12b973736a0c33cdc6f235ace
Jake Fishbain <jakefishbain@gmail.com>
2021-06-21 14:01:28 -0400

    add db config

---
+++ db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  HOST: process.env.HOST || 'localhost',
+  USER: process.env.DB_USER || 'root',
+  PASSWORD: process.env.DB_PASSWORD || '',
+  DB: process.env.DB || 'dad_joke_sender'
+};
